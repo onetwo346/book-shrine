65b70be4bb64b5148dcaeb420af06d167476ed30
CosmosCoderr <lightwhite207@gmail.com>
2025-03-05 17:23:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -51,18 +51,8 @@ const books = [
     filePath: "The-silent-Achitect.pdf",
   },
 
-  { 
-    title: "The Algorithm Of Souls",
-    author: "Cosmos Coderr",
-    description: "Science Fiction/Mystery.",
-    filePath: "The-Algorithm-Of-Souls.pdf",
 
 
-
-
-
-
-  
 ];
 
 const introPage = document.getElementById("intro-page");
